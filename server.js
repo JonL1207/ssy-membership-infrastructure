066a83c9251f1cdd048bd92c3d77cdd45635e720
Jon Law <jonlawuk@yahoo.co.uk>
2023-02-17 21:24:26 +0000

    Configured server.js with dependencies the server will use, the middleware needed to process requests and store sessions, the view engine the application will be using, the connection to the database and  the routes users of the application will visit

---
+++ server.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const memberRoutes = require('./routes/memberRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const store = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    collection: 'sessions'
+});
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false, //set to true in production for https only access!
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 //cookie expires after 1 hour
+    },
+    store: store
+}));
+app.use(morgan('dev'));
+
+// View Engine
+app.set('view engine', 'ejs');
+
+// Database Connection
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB!'))
+    .catch((err) => console.log(err));
+
+//Routes
+app.get('/', (req, res) => res.render('index'));
+//app.use('/members', memberRoutes);
+//app.use('/admin', adminRoutes);
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
